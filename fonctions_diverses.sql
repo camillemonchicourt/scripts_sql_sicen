9bfeb4f81037eeba1adf127f1a2b74bb2ed38793
mathieubossaert <mathieu.bossaert@cenlr.org>
2015-04-23 19:40:40 +0200

    Update fonctions_diverses.sql

--- fonctions_diverses.sql
+++ fonctions_diverses.sql
@@ -11,3 +11,17 @@ END;
 $BODY$
   LANGUAGE plpgsql IMMUTABLE
   COST 100;
+
+CREATE OR REPLACE FUNCTION md.liste_nom_structure(text, text)
+  RETURNS text AS
+$BODY$
+DECLARE
+    var_liste_code_structure ALIAS for $1;
+    separateur ALIAS for $2;
+BEGIN
+RETURN string_agg(nom_structure,separateur) FROM (SELECT regexp_split_to_table(var_liste_code_structure,'&')::integer as id_structure) t
+LEFT JOIN md.structure USING(id_structure);
+END;
+$BODY$
+  LANGUAGE plpgsql VOLATILE
+  COST 100;
